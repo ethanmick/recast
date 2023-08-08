5b01797f92967fd674b929a816323839ec149f8e
Ethan Mick <ethan@ethanmick.com>
2023-08-08 15:26:06 -0400

    fix: Conversions breaking do not record errors

--- converter/index.ts
+++ converter/index.ts
@@ -14,55 +14,69 @@ AWS.config.update({
 const bucket = process.env.S3_BUCKET_NAME!
 
 const convert = async (c: Conversion) => {
-  const s3 = new AWS.S3()
-  const downloadParams = {
-    Bucket: bucket,
-    Key: c.s3Key,
-  }
-  console.log(`Downloading File`, downloadParams)
-  const res = await s3.getObject(downloadParams).promise()
+  try {
+    const s3 = new AWS.S3()
+    const downloadParams = {
+      Bucket: bucket,
+      Key: c.s3Key,
+    }
+    console.log(`Downloading File`, downloadParams)
+    const res = await s3.getObject(downloadParams).promise()
 
-  const converters = findPath(c.fromMime, c.toMime)
-  if (!converters) {
-    console.error(`Could not find converters for ${c.fromMime} to ${c.toMime}`)
+    const converters = findPath(c.fromMime, c.toMime)
+    if (!converters) {
+      console.error(
+        `Could not find converters for ${c.fromMime} to ${c.toMime}`
+      )
+      await prisma.conversion.update({
+        where: {
+          id: c.id,
+        },
+        data: {
+          error: `Could not convert from ${c.fromMime} to ${c.toMime}`,
+          status: ConversionStatus.ERROR,
+        },
+      })
+      return
+    }
+
+    let converted = res.Body as Buffer
+    for (const edge of converters) {
+      converted = await edge.converter(res.Body as Buffer)
+    }
+
+    console.log('Lookup', converters[converters.length - 1].to.type)
+    const mime = extension(converters[converters.length - 1].to.type) as string
+
+    const key = (randomUUID() + randomUUID()).replace(/-/g, '')
+    console.log(`Uploading to`, key)
+    const uploadParams = {
+      Bucket: bucket,
+      Key: key,
+      Body: converted,
+    }
+    await s3.upload(uploadParams).promise()
     await prisma.conversion.update({
       where: {
         id: c.id,
       },
       data: {
-        error: `Could not convert from ${c.fromMime} to ${c.toMime}`,
-        status: ConversionStatus.ERROR,
+        status: ConversionStatus.DONE,
+        s3Key: key,
+        currentMime: mime,
       },
     })
-    return
+  } catch (err: any) {
+    await prisma.conversion.update({
+      where: {
+        id: c.id,
+      },
+      data: {
+        status: ConversionStatus.ERROR,
+        error: `Could not convert: ${err?.message}`,
+      },
+    })
   }
-
-  let converted = res.Body as Buffer
-  for (const edge of converters) {
-    converted = await edge.converter(res.Body as Buffer)
-  }
-
-  console.log('Lookup', converters[converters.length - 1].to.type)
-  const mime = extension(converters[converters.length - 1].to.type) as string
-
-  const key = (randomUUID() + randomUUID()).replace(/-/g, '')
-  console.log(`Uploading to`, key)
-  const uploadParams = {
-    Bucket: bucket,
-    Key: key,
-    Body: converted,
-  }
-  await s3.upload(uploadParams).promise()
-  await prisma.conversion.update({
-    where: {
-      id: c.id,
-    },
-    data: {
-      status: ConversionStatus.DONE,
-      s3Key: key,
-      currentMime: mime,
-    },
-  })
 }
 
 const main = async () => {
