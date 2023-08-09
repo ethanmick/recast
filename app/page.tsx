6c2d18d0aac1b68da6b08b79781a350b35261b49
Ethan Mick <ethan@ethanmick.com>
2023-08-08 20:21:58 -0400

    feat: Blur on header

--- app/page.tsx
+++ app/page.tsx
@@ -55,9 +55,11 @@ export default function Home() {
     <Dropzone onDrop={onDrop}>
       {({ open }) => (
         <>
-          <header className="flex items-center mx-auto container p-2">
-            <Image src="/icon.png" width={64} height={64} alt="Recast Logo" />
-            <div className="text-xl font-semibold ml-4">Recast</div>
+          <header className="sticky top-0 backdrop-blur bg-white/80">
+            <div className="flex items-center">
+              <Image src="/icon.png" width={64} height={64} alt="Recast Logo" />
+              <div className="text-xl font-semibold ml-4">Recast</div>
+            </div>
           </header>
           <main className="container mx-auto">
             <Hero open={open} />
@@ -73,6 +75,35 @@ export default function Home() {
               euismod, sapien vel bibendum bibendum, velit sapien bibendum
               sapien, vel bibendum sapien sapien vel sapien. Sed 0
             </p>
+            <section>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                aliquet nulla a dui pharetra, nec viverra nisl sollicitudin.
+                Fusce ac orci non nunc tristique tincidunt. Etiam euismod, felis
+                eu gravida sollicitudin, erat est dictum lectus, id iaculis
+                justo enim nec erat. Nunc nec bibendum nunc. Proin sed nisi quis
+                augue tincidunt scelerisque eu a quam. Vestibulum quis neque
+                eget felis suscipit pellentesque non non dui.
+              </p>
+              <p>
+                Vivamus et neque eu dui facilisis dapibus. Nullam bibendum id
+                libero at bibendum. Mauris a ligula nec turpis tincidunt
+                vestibulum. Phasellus id metus dolor. Maecenas vel neque ut nisi
+                porttitor scelerisque. Mauris in felis vel nisl laoreet egestas
+                nec at ligula. Sed sed eros quis enim varius fermentum.
+                Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
+                posuere cubilia curae; Proin sagittis erat in nisl aliquet, sit
+                amet iaculis purus bibendum.
+              </p>
+              <p>
+                Cras sed sem sed diam ullamcorper commodo a nec erat. In hac
+                habitasse platea dictumst. Sed sit amet mollis tortor. Vivamus
+                in tellus a sapien fermentum auctor id quis justo. Sed in quam
+                vel ex congue faucibus sit amet non dui. Proin mollis neque nec
+                diam malesuada, et condimentum libero ultrices. Morbi eu orci
+                vel lectus facilisis pellentesque.
+              </p>
+            </section>
           </main>
         </>
       )}
