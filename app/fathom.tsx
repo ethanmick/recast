5eb6a0a20b5182ecfe2210d4faf06d9c1dfd0fae
Ethan Mick <ethan@ethanmick.com>
2023-08-09 11:30:27 -0400

    feat: Analytics

---
+++ app/fathom.tsx
@@ -0,0 +1,36 @@
+'use client'
+
+import { load, trackPageview } from 'fathom-client'
+import { usePathname, useSearchParams } from 'next/navigation'
+import { Suspense, useEffect } from 'react'
+
+function TrackPageView() {
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+
+  useEffect(() => {
+    load('BLOEMVLV', {
+      includedDomains: ['www.recastfile.com', 'recastfile.com'],
+    })
+  }, [])
+
+  // Record a pageview when route changes
+  useEffect(() => {
+    if (!pathname) return
+
+    trackPageview({
+      url: pathname + searchParams.toString(),
+      referrer: document.referrer,
+    })
+  }, [pathname, searchParams])
+
+  return null
+}
+
+export default function Fathom() {
+  return (
+    <Suspense fallback={null}>
+      <TrackPageView />
+    </Suspense>
+  )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Fathom from './fathom'
 import './globals.css'
 import { Providers } from './providers'
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Fathom />
         <Providers>{children}</Providers>
       </body>
     </html>
