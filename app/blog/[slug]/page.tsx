16e5e044742af4c58c22e66baea8a3a5d9a8af0a
Ethan Mick <ethan@ethanmick.com>
2023-08-16 14:02:39 -0400

    feat: Meta tags for blog posts

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,11 +1,32 @@
 import { Meta } from '@/lib/blog'
 import { readdir } from 'fs/promises'
+import { Metadata, ResolvingMetadata } from 'next'
 import dynamic from 'next/dynamic'
 import Image from 'next/image'
 import { join } from 'path'
 
 type Props = {
   params: { slug: string }
+}
+
+export async function generateMetadata(
+  { params }: Props,
+  parent?: ResolvingMetadata
+): Promise<Metadata> {
+  const { meta }: { meta: Meta } = require(`../../../blog/${params.slug}.mdx`)
+  return {
+    title: meta.title,
+    description: meta.excerpt,
+    openGraph: {
+      images: [meta.image],
+      type: 'website',
+      description: meta.excerpt,
+      url: `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/blog/${params.slug}}`,
+    },
+    twitter: {
+      card: 'summary_large_image',
+    },
+  }
 }
 
 export default async function ({ params }: Props) {
