705f31d1da9e3d526d60ca068da993fe3d0a3eeb
Ethan Mick <ethan@ethanmick.com>
2023-08-22 13:17:18 -0400

    feat: Add additional image types

--- converter/converters/image/nodes.ts
+++ converter/converters/image/nodes.ts
@@ -20,6 +20,54 @@ const IMAGE_GIF: MimeNode = {
 }
 nodes.push(IMAGE_GIF)
 
+// BMP
+const IMAGE_BMP: MimeNode = {
+  mime: 'image/bmp',
+}
+nodes.push(IMAGE_BMP)
+
+// TIFF
+const IMAGE_TIFF: MimeNode = {
+  mime: 'image/tiff',
+}
+nodes.push(IMAGE_TIFF)
+
+// RAW??
+// const IMAGE_TIFF: MimeNode = {
+//   mime: 'image/tiff',
+// }
+// nodes.push(IMAGE_TIFF)
+
+// PSD
+const IMAGE_VND_ADOBE_PHOTOSHOP: MimeNode = {
+  mime: 'image/vnd.adobe.photoshop',
+}
+nodes.push(IMAGE_VND_ADOBE_PHOTOSHOP)
+
+// SVG
+const IMAGE_SVG_XML: MimeNode = {
+  mime: 'image/svg+xml',
+}
+nodes.push(IMAGE_SVG_XML)
+
+// webp
+const IMAGE_WEBP: MimeNode = {
+  mime: 'image/webp',
+}
+nodes.push(IMAGE_WEBP)
+
+// heif
+const IMAGE_HEIF: MimeNode = {
+  mime: 'image/heif',
+}
+nodes.push(IMAGE_HEIF)
+
+// heic
+const IMAGE_HEIC: MimeNode = {
+  mime: 'image/heic',
+}
+nodes.push(IMAGE_HEIC)
+
 // ICO
 const IMAGE_X_ICON: MimeNode = {
   mime: 'image/x-icon',
@@ -28,6 +76,24 @@ const IMAGE_X_ICON: MimeNode = {
   },
 }
 nodes.push(IMAGE_X_ICON)
+
+// jp2
+const IMAGE_JP2: MimeNode = {
+  mime: 'image/jp2',
+}
+nodes.push(IMAGE_JP2)
+nodes.push({
+  mime: 'image/jpx',
+})
+nodes.push({
+  mime: 'image/jpm',
+})
+
+// // jxl
+// const IMAGE_JXL: MimeNode = {
+//   mime: 'image/jxl',
+// }
+// nodes.push(IMAGE_JXL)
 
 // PDF
 const APPLICATION_PDF: MimeNode = {
